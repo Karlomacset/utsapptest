6600c5089e4cd83204aafe379d40e892356ff537
Karlomac <karlo@crimsontechnik.com>
2020-03-28 23:53:01 +0800

    intial updated and upload

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('userDB');   
+            $table->string('userDB')->nullable(); 
+            $table->string('dispatchDB')->nullable();  
             $table->rememberToken();
             $table->timestamps();
         });
